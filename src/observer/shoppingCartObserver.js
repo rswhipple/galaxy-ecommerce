b67fee51cea5aab437ddca5a38237729a0837a58
Rebecca W Silverstein <rswhipple@gmail.com>
2024-08-24 12:53:56 -0500

    change WeatherStation to ShoppingCart and temperature to cart

--- src/observer/shoppingCartObserver.js
+++ src/observer/shoppingCartObserver.js
@@ -3,8 +3,8 @@ class ShoppingCart {
     constructor() {
         // Initialize an empty array to hold observers
         this.observers = [];
-        // Initialize the temperature to 0
-        this.temperature = 0;
+        // Initialize the cart to empty aka 0 items
+        this.cart = 0;
     }
     
     // Method to add an observer to the list
@@ -17,17 +17,17 @@ class ShoppingCart {
         this.observers = this.observers.filter(obs => obs !== observer);
     }
     
-    // Method to set the temperature and notify observers
-    setTemperature(temperature) {
-        this.temperature = temperature;
+    // Method to set the cart and notify observers
+    addToCart(cart) {
+        this.cart = cart;
         this.notifyObservers();
     }
     
-    // Method to notify all observers about the temperature change
+    // Method to notify all observers about the cart change
     notifyObservers() {
         this.observers.forEach(observer => {
         // Call the update method on each observer
-        observer.update(this.temperature);
+        observer.update(this.cart);
         });
     }
     }
@@ -39,27 +39,27 @@ class ShoppingCart {
         this.name = name;
     }
     
-    // Method called when the display is updated with temperature
-    update(temperature) {
-        // Log a message indicating the name of the display and the temperature
-        console.log(`${this.name} Display: Temperature is ${temperature}°C`);
+    // Method called when the display is updated with cart
+    update(cart) {
+        // Log a message indicating the name of the display and the cart
+        console.log(`${this.name} Display: Cart is ${cart}°C`);
     }
     }
     
     // Create an instance of the WeatherStation class
-    const weatherStation = new WeatherStation();
+    const shoppingCart = new ShoppingCart();
     
     // Create two instances of the DisplayDevice class with different names
     const displayDevice1 = new DisplayDevice("Display 1");
     const displayDevice2 = new DisplayDevice("Display 2");
     
     // Add both display devices as observers to the weather station
-    weatherStation.addObserver(displayDevice1);
-    weatherStation.addObserver(displayDevice2);
+    shoppingCart.addObserver(displayDevice1);
+    shoppingCart.addObserver(displayDevice2);
     
-    // Simulate a change in temperature by setting it to 25°C
-    weatherStation.setTemperature(25);
+    // Simulate a change in cart by setting it to 25°C
+    shoppingCart.setCart(25);
     
-    // Simulate another change in temperature by setting it to 30°C
-    weatherStation.setTemperature(30);
+    // Simulate another change in cart by setting it to 30°C
+    shoppingCart.setCart(30);
     
